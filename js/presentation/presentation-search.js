4e5cc54a0c4f70466059383117c7576194ddda3e
Axel Smeets <dat12asm@student.lu.se>
2018-05-11 09:57:34 +0200

    merge in presentation

---
+++ js/presentation/presentation-search.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+
+    //hides collection selector but still needed as page has dependencies on it.
+    document.getElementById('collection-dropdown').style.display='none'
+    document.getElementById('sort-dropdown').style.display='none'
+
+})
